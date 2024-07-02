f240659462f18758ff5d2f79ad522e3b480a64d5
Jean Pablo Carvalho da Silva <jpablo1026@hotmail.com>
2024-07-02 05:49:53 -0300

    setando default class1

--- BeecrowdProblems/Program.cs
+++ BeecrowdProblems/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        /**
+         * Escreva a sua solução aqui
+         * Code your solution here
+         * Escriba su solución aquí
+         */
+
+    }
+
+}
